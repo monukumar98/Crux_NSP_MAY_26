c812bf3bf77bb2a3c86407247dd1df3da0461916
monukumar98 <monu012as@gmail.com>
2026-05-29 13:36:41 +0530

    flow chart

---
+++ Lec0/Test.java
@@ -0,0 +1,13 @@
+package Lec0;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Coidng Blocks");
+		System.out.print("hey");
+		System.out.println("hello");
+
+	}
+
+}
