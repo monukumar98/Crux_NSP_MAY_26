b2f0abfe2f7046c9b8902fd83822dc2b12d1fa1d
monukumar98 <monu012as@gmail.com>
2026-06-08 14:21:38 +0530

    Function and Array

--- Lec4/Range_Reverse_Array.java
+++ Lec4/Range_Reverse_Array.java
@@ -4,15 +4,14 @@ public class Range_Reverse_Array {
 
 	public static void main(String[] args) {
 		int[] arr = { 3, 5, 6, 2, 4, 16, 7, 8, 9 };
-		Reverse(arr);
+		Reverse(arr, 2, 6);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 	}
 
-	public static void Reverse(int[] arr) {
-		int i = 0;
-		int j = arr.length - 1;
+	public static void Reverse(int[] arr, int i, int j) {
+
 		while (i < j) {
 			int temp = arr[i];
 			arr[i] = arr[j];
